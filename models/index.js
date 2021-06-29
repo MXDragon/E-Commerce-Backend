b06b5eb2dfba8ab81d54aa8a16de15087dddc46e
hurondragon <bentzbry@gmail.com>
2021-06-29 16:51:53 -0400

    Adding Product belongs to many

--- models/index.js
+++ models/index.js
@@ -12,6 +12,11 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
